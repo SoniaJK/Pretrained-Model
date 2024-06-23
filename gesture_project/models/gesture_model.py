6167e5be63186d82664a044f9a721736b9cafc44
SoniaJK <142544829+SoniaJK@users.noreply.github.com>
2024-06-23 00:49:29 -0400

    Update gesture_model.py

--- gesture_project/models/gesture_model.py
+++ gesture_project/models/gesture_model.py
@@ -1,7 +1,7 @@
 from src.backbone import TFLiteModel, get_model
 
 def load_model():
-    model_path = './model/your_model.h5'  # Adjust the path if necessary
+    model_path = '/Pretrained-Model/gesture_project/models/islr-fp16-192-8-seed42-fold0-best.h5'  # Adjust the path if necessary
     model = get_model()
     model.load_weights(model_path)
     return model
